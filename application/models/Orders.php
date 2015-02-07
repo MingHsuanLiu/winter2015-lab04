83ed37b0730da6af05b4c93c08c9563bcea14de0
A00770012 <A00770012bcit@gmail.com>
2015-02-07 14:58:14 -0800

    changed the megthod of total to return total amount of the order Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -19,7 +19,23 @@ class Orders extends MY_Model {
 
     // calculate the total for an order
     function total($num) {
-        return 0.0;
+        // change the total amount for an order by iterate over the items in an order
+        $CI = &get_instance();
+        $CI->load->model('orderitems');
+        
+        // get all the items in the order by calling some('code',$num)
+        $items = $this->orderitems->some('code', $num);
+        
+        // used foreach loop to iterate all the item in items and add them up
+        $result = 0.0;
+        foreach ($items as $item) 
+        {
+            $menuitem = $this->menu->get($item->item);
+            $result = $item->quantity * $menuitem->price;
+        }
+        
+        // return total amount of items in the order
+        return $result;
     }
 
     // retrieve the details for an order
