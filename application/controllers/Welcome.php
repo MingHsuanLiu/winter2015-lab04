6ed349b20bc276a8eb6e70b3e7f50762634c28cf
A00770012 <A00770012bcit@gmail.com>
2015-02-07 13:35:22 -0800

    fixed the autoload.php to load database library Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,6 +25,9 @@ class Welcome extends Application {
 
         // Get all the completed orders
         //FIXME
+        // added variable $completed to contain the order data
+        $completed = $this->orders->some('status', 'c');
+
 
         // Build a multi-dimensional array for reporting
         $orders = array();
